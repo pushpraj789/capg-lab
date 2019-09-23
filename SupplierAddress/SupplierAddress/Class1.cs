d2ac388225c578dac58d5cd45c7bf9a1e9e779d6
pushpraj789 <55019385+pushpraj789@users.noreply.github.com>
2019-09-23 18:47:23 +0530

    Add files via upload

---
+++ SupplierAddress/SupplierAddress/Class1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Capgemini.Inventory.Helpers.ValidationAttributes;
+
+namespace Capgemini.Inventory.Entities
+{
+    /// <summary>
+    /// Interface for SystemUser Entity
+    /// </summary>
+    public interface ISupplierAddress
+    {
+        Guid AddressID { get; set; }
+        string UserType { get; set ; }
+        Guid SupplierID { get; set; }
+        string AddressLine1 { get; set; }
+        string AddressLine2 { get; set; }
+        string PinCode { get; set; }
+        string City { get; set; }
+        string State { get; set; }
+    }
+}
+
+    /// <summary>
+    /// Represents SystemUser
+    /// </summary>
+    public class SupplierAddress : ISupplierAddress, IUser
+    {
+        /* Auto-Implemented Properties */
+        [Required("Address ID can't be blank.")]
+        [RegExp(@"^(\w{2,40})$", "SystemUser Name should contain only 2 to 40 characters.")]
+        public Guid AddressID { get; set; }
+
+        [Required("User Type should be supplier or  distributor")]
+        public string UserType{ get; set; }
+
+        [Required("AddressLine1 should be supplier or  distributor")]
+        public string AddressLine1{ get; set; }
+
+        [Required("AddressLine2 should not be blank")]
+        public string AddressLine2 { get; set; }
+
+        [Required("PinCode should not be blank")]
+        public string PinCode{ get; set; }
+
+        [Required("City should not be blank")]
+         public string City{ get; set; }
+
+        [Required("State should not be blank")]
+        public string State { get; set; }
+
+        [Required("Address ID can't be blank.")]
+        [RegExp(@"^(\w{2,40})$", "SystemUser Name should contain only 2 to 40 characters.")]
+        public Guid SupplierID { get; set; }
+
+
+    public SupplierAddress()
+         {
+               AddressID = default(Guid);
+               UserType = string.Empty;
+               AddressLine1 = string.Empty;
+               AddressLine2 = string.Empty;
+               PinCode = string.Empty;
+               City = string.Empty;
+               State = string.Empty;
+               SupplierID = default(Guid);
+         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+    }
+}
+
+
+
---
+++ SupplierAddress/SupplierAddress/Class2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupplierAddress
+{
+   using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Entities
+{
+    public class AddressSup
+    {
+        //fields
+        private string addressID;
+        private string userType;
+        private string addressLine1;
+        private string addressLine2;
+        private string pinCode;
+        private string city;
+        private string state;
+        private string supplierID;
+
+        //properties
+        public string AddressID { get => addressID; set => addressID = value; }
+        public string UserType { get => userType; set => userType = value; }
+        public string AddressLine1 { get => addressLine1; set => addressLine1 = value; }
+        public string AddressLine2 { get => addressLine2; set => addressLine2 = value; }
+        public string PinCode { get => pinCode; set => pinCode = value; }
+        public string City { get => city; set => city = value; }
+        public string State { get => State1; set => State1 = value; }
+        public string State1 { get => state; set => state = value; }
+        public string SupplierID { get => supplierID; set => supplierID = value; }
+
+
+        //Constructor
+        public AddressSup()
+        {
+            AddressID = string.Empty;
+            UserType = string.Empty;
+            AddressLine1 = string.Empty;
+            AddressLine2 = string.Empty;
+            PinCode = string.Empty;
+            City = string.Empty;
+            State = string.Empty;
+            SupplierID = string.Empty;
+        }
+
+       
+    }
+}
+
+}
